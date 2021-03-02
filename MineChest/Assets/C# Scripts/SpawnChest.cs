3175ff7e820f7ef1e2858ca438069915b94855a5
Modestas <modestaskris@gmail.com>
2021-03-02 22:14:57 +0200

    Pridetas pradinis chestu spawninimo algoritmas

---
+++ MineChest/Assets/C# Scripts/SpawnChest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnChest : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject chest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Spawns randomly everytime game starts
+        for (int i = 0; i < 10; i++)
+        {
+            Spawn();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        Instantiate(chest, new Vector3(Random.Range(-50f, 50f), Random.Range(-35f,35f), 0f), Quaternion.identity);
+    }
+}
