6f231c51f23cf03e77aae259f46141b6ecdc15ae
Dovydas Jurgutis <dovydas.jurgutis@ktu.edu>
2021-04-07 21:32:38 +0300

    Chestas pakeičia vietą po atsidarymo animacijos

--- MineChest/Assets/C# Scripts/DigUpChest.cs
+++ MineChest/Assets/C# Scripts/DigUpChest.cs
@@ -4,37 +4,63 @@ using UnityEngine;
 
 public class DigUpChest : MonoBehaviour
 {
-    [SerializeField]
-    private SpriteRenderer renderer;
-    [SerializeField]
-    private BoxCollider2D chest;
-    [SerializeField]
-    private BoxCollider2D player;
-    [SerializeField]
-    private Animator animator;
+    private SpriteRenderer chestRenderer;
+    private BoxCollider2D chestColider;
+    private BoxCollider2D playerColider;
+    private Animator playerAnimator;
+    private Animator chestAnimator;
+    private bool moveChest = false;
 
+    private void Start()
+    {
+        playerColider = GetComponent<BoxCollider2D>();
+        playerAnimator = GetComponent<Animator>();
 
+        chestRenderer = GameObject.Find("Chest").GetComponent<SpriteRenderer>();
+        chestColider = GameObject.Find("Chest").GetComponent<BoxCollider2D>();
+        chestAnimator = GameObject.Find("Chest").GetComponent<Animator>();
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
+        DigChest();
         
-        if (Input.GetKey("f"))
+        if (Input.GetKey("g") && chestRenderer.enabled && playerColider.bounds.Intersects(chestColider.bounds)) // to open chest, possible only if player intersects chest bounds, press g and chest is digged
         {
-            animator.SetBool("dig", true);
-            if (player.bounds.Intersects(chest.bounds))
-            {
-                renderer.enabled = true;
-            }
+            chestAnimator.SetBool("IsOpen", true);
         }
-        else if (Input.GetKey("g") && renderer.enabled && player.bounds.Intersects(chest.bounds)) // to open chest, possible only if player intersects chest bounds, press g and chest is digged
+
+
+        if (chestAnimator.GetCurrentAnimatorStateInfo(0).IsTag("1") && chestAnimator.GetBool("IsOpen"))
+        {
+            chestAnimator.SetBool("IsOpen", false);
+            moveChest = true;
+        }
+
+        if (chestAnimator.GetCurrentAnimatorStateInfo(0).IsTag("2") && moveChest)
         {
             GameObject.Find("Chest").GetComponent<SpawnChest>().isOpened = true;
+            moveChest = false;
         }
 
+
+
+
+    }
+
+    void DigChest()
+    {
+        if (Input.GetKey("f"))
+        {
+            playerAnimator.SetBool("dig", true);
+            if (playerColider.bounds.Intersects(chestColider.bounds))
+            {
+                chestRenderer.enabled = true;
+            }
+        }
         else
         {
-            animator.SetBool("dig", false);
+            playerAnimator.SetBool("dig", false);
         }
-
     }
 }
