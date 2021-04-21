b62bb559d5baa4924ee89963a2d845d3539d6355
Dovydas Jurgutis <dovydas.jurgutis@ktu.edu>
2021-04-21 15:23:29 +0300

    Sutvarkytas minimap dizainas ir chesto colideriai

--- MineChest/Assets/C# Scripts/Minimap.cs
+++ MineChest/Assets/C# Scripts/Minimap.cs
@@ -15,6 +15,7 @@ public class Minimap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKey("m"))
         {
             minimap.enabled = true;
