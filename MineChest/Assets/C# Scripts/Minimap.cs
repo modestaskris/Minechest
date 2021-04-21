4d3646b26c7b8a26667913b89bfbb12cffa9fce0
Dovydas Jurgutis <dovydas.jurgutis@ktu.edu>
2021-04-21 10:45:30 +0300

    Pirdėtas minimapas

---
+++ MineChest/Assets/C# Scripts/Minimap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    private Camera minimap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        minimap = GetComponent<Camera>();
+        minimap.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("m"))
+        {
+            minimap.enabled = true;
+            GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = false;
+        }
+        else
+        {
+            minimap.enabled = false;
+            GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
+        }
+
+    }
+}
