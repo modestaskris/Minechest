6a6df685ff24f67e68803934a63e209cbe96f311
Matas Suslavičius <matas.suslavicius@ktu.edu>
2021-05-05 19:15:31 +0300

    sutvarkyta klaida su chestdatabase

---
+++ MineChest/Assets/C# Scripts/ChestShopDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ChestShopDatabase", menuName = "Shopping/Chests shop database")]
+public class ChestShopDatabase : ScriptableObject
+{
+    public Chest[] chests;
+
+    public int ChestsCount
+    {
+        get { return chests.Length; }
+    }
+
+    public Chest GetChest(int index)
+    {
+        return chests[index];
+    }
+
+    //public void PurchaseItem(int index)
+    //{
+    //    chests[index].isPurchased = true;
+    //}
+}
--- MineChest/Assets/C# Scripts/GameDataManager.cs
+++ MineChest/Assets/C# Scripts/GameDataManager.cs
@@ -3,7 +3,7 @@
 [System.Serializable]
 public class PlayerData
 {
-    public int coins = 0;
+    public int coins = 1000;
 }
 //Shop Data Holder
 [System.Serializable]
