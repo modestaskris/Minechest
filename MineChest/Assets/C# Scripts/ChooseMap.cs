c138ab0d465ed64e6edca42679bf85f8890c7192
Dovydas Jurgutis <dovydas.jurgutis@ktu.edu>
2021-05-07 20:20:26 +0300

    Pridėtas žemėlapių pasirinkimas

---
+++ MineChest/Assets/C# Scripts/ChooseMap.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseMap : MonoBehaviour
+{
+    public void ChangeScene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }   
+}
